bcd66a4059be29dced27e4451f1c9aac86a6fb38
Cesar <cesar@localhost>
2023-04-04 20:07:23 -0300

    Initial commit Começando a calculadora

---
+++ Calculadora.py
@@ -0,0 +1,11 @@
+a = 10
+b = 5
+a = int(input("Digite o primeiro valor: "))
+b = int(input("Digite o segundo valor: "))
+soma = a + b
+subtração = a - b
+multiplicação = a * b
+divisão = a / b
+resto = a % b
+resultado = print("Soma: {soma} \n Subtração: {subtração} \n Multiplicação: {multiplicação} \n Divisão: {divisão} \n Resto: {resto}".format(soma=soma, subtração=subtração, divisão=divisão, resto=resto, multiplicação=multiplicação))
+print(resultado)
