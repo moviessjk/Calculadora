693904fc5a9481eb00559da1687d05839a9ea461
Cesar <cesar@localhost>
2023-04-12 18:44:17 -0300

    upgrade

--- Calculadora.py
+++ Calculadora.py
@@ -1,5 +1,3 @@
-a = 10
-b = 5
 a = int(input("Digite o primeiro valor: "))
 b = int(input("Digite o segundo valor: "))
 soma = a + b
@@ -7,5 +5,5 @@ subtracao = a - b
 multiplicacao = a * b
 divisao = a / b
 resto = a % b
-resultado = print("Soma: {soma}, Subtracao: {subtracao}, Multiplicacao: {multiplicacao}, Divisao: {divisao}, Resto: {resto}".format(soma=soma, subtracao=subtracao, divisao=divisao, resto=resto, multiplicacao=multiplicacao))
+resultado = print(f"Soma: {soma}, Subtracao: {subtracao}, Multiplicacao: {multiplicacao}, Divisao: {divisao}, Resto: {resto}")
 print = str(resultado)
